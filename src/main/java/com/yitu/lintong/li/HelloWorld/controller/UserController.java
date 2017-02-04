4d228ab8e69078667ae300d7e9de0f87b296c3ef
lilintong22222 <269255675@qq.com>
2017-02-04 20:44:00 +0800

    rm test

--- src/main/java/com/yitu/lintong/li/HelloWorld/controller/UserController.java
+++ src/main/java/com/yitu/lintong/li/HelloWorld/controller/UserController.java
@@ -23,10 +23,4 @@ public class UserController {
         model.addAttribute("user", user);
         return "userShow";
     }
-
-    @ResponseBody
-    @RequestMapping("/test")
-    public String test() {
-        return "Hello World";
-    }
 }
